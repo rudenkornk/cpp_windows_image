0b42117626bb5698cc7796e89c37302a4b2018ed
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-02 13:55:59 +0300

    Add python3 symlink

--- install_python.ps1
+++ install_python.ps1
@@ -1,6 +1,7 @@
 choco install python --version=3.10.4
 
 $env:path = "$env:path;C:\Python310;C:\Python310\Scripts"
+New-Item -Path "C:\Python310\python3.exe" -ItemType SymbolicLink -Value "C:\Python310\python.exe"
 
 python -m pip install -U pip
 
