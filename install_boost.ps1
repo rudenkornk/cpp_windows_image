d5a6c30324c1533c7da0d1cc872abc1db412bbcd
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-10 14:47:11 +0300

    Fix wrong path to boost libs

--- install_boost.ps1
+++ install_boost.ps1
@@ -17,6 +17,6 @@ $new_include = "$current_include;C:\Program Files\boost\boost_1_79_0\"
 [Environment]::SetEnvironmentVariable("INCLUDE", $new_include, "Machine")
 
 $current_lib = [Environment]::GetEnvironmentVariable("LIB", "Machine");
-$new_lib = "$current_lib;C:\Program Files\boost\boost_1_79_0\stage\lib"
+$new_lib = "$current_lib;C:\Program Files\boost\boost_1_79_0\lib"
 [Environment]::SetEnvironmentVariable("LIB", $new_lib, "Machine")
 
