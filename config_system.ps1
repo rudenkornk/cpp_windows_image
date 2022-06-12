5dc6d680d16d7d8c61450bcdf440da4160885863
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-12 20:07:12 +0300

    Add bash to path

--- config_system.ps1
+++ config_system.ps1
@@ -1,3 +1,7 @@
 choco install vim
 choco install make
 
+# Support bash -C
+$current_path = [Environment]::GetEnvironmentVariable("PATH", "Machine");
+$new_path = "C:\Program Files\Git\bin;$current_path"
+[Environment]::SetEnvironmentVariable("PATH", $new_path, "Machine")
