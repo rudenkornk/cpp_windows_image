c653b4feeb99f8a308848658e69fb484dff6567e
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-17 16:09:00 +0300

    Add wheel python package

--- install_python.ps1
+++ install_python.ps1
@@ -4,4 +4,5 @@ $env:path = "$env:path;C:\Python310;C:\Python310\Scripts"
 New-Item -Path "C:\Python310\python3.exe" -ItemType SymbolicLink -Value "C:\Python310\python.exe"
 
 python -m pip install -U pip
+pip install wheel
 
