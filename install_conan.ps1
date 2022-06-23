d5e86d285f2d6fe82b03329dbe4e4cff71568db2
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-23 23:07:46 +0300

    [DEPLOY] Enable long paths

--- install_conan.ps1
+++ install_conan.ps1
@@ -1,3 +1,5 @@
 choco install yq
 
 pip install conan==1.49.0
+
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
